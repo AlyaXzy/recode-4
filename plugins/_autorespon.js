1932729ab471c2f41d4c564deead32db0c12756b
BunyanXd <101381544+BunyanXd@users.noreply.github.com>
2022-03-25 21:56:17 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -72,7 +72,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} ⏲️| Mode: ${global.opts['self'] ? 'Private 🛌' : setting.groupOnly ? 'Hanya Grup' : 'Publik 👾'} |YanZBotZ🌾 by Bunyan-Gans 🥀`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} ⏲️| Mode: ${global.opts['self'] ? 'Private 🛌' : setting.groupOnly ? 'Hanya Grup' : 'Publik 👾'} |LiaBotZ🗿 By Bunyan-Gans 🥀`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
